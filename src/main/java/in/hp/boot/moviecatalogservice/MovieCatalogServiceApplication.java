9ed3e657950298ab5ea3e075ea930e11b2b99e93
Hariprasath <balaprasad24@gmail.com>
2020-05-26 00:34:15 +0530

    Added hystrix dependency

--- src/main/java/in/hp/boot/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/in/hp/boot/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package in.hp.boot.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class MovieCatalogServiceApplication {
 
     public static void main(String[] args) {
